044c99796530a34c77b14646228c154990fb0c75
Alabi Olalekan Emmanuel <alabilekanemmanuel@gmail.com>
2022-05-10 01:26:23 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const swiper = new Swiper(".swiper", {
+    autoplay: {
+        delay: 3000,
+        diasbleOnInteraction: false,
+    },
+  loop: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: 'true',
+  },
+
+  
+
+  
+});
+
+let hamburgerIcon = document.getElementById("hamburger-icon");
+let navLinks = document.getElementById('nav-links');
+
+hamburgerIcon.addEventListener('click',() =>{
+  navLinks.classList.toggle('active');
+});
+
+console.log(navLinks);
